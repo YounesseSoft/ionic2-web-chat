36cf95adf13eee8ec93b1b1616ed700eaf190eb9
david <beetz12@gmail.com>
2016-06-30 11:36:29 -0400

    added socket-service

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,7 @@
 import { Component }                    from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {HomePage} from './pages/home/home';
-
+import {SocketService} from './providers/socket-service/socket-service';
 
 @Component({
   templateUrl: 'build/app.html'
@@ -11,7 +11,7 @@ import {HomePage} from './pages/home/home';
 
 export class MyApp {
   root: any;
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, public socket: SocketService) {
     this.initializeApp();
     this.root = HomePage;
   }
@@ -20,9 +20,14 @@ export class MyApp {
     this.platform.ready().then(() => {
       console.log('Platform ready');
     });
+
+     this.socket.initialize();
+     this.socket.socketService.subscribe(event => {
+         console.log('message received from server... ', event);
+     });
   }
 }
 
-ionicBootstrap(MyApp, [], {
+ionicBootstrap(MyApp, [SocketService], {
   mode: 'ios'
 });
